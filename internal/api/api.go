5eb6e09122bba2084363b6f72e777aa5d5767364
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-08-06 14:33:42 -0300

    feat: implementing notify clients method

--- internal/api/api.go
+++ internal/api/api.go
@@ -74,6 +74,30 @@ func NewHandler(q *pgstore.Queries) http.Handler {
 	return a
 }
 
+type Message struct {
+	Kind   string `json:"kind"`
+	Value  any    `json:"value"`
+	RoomID string `json:"-"`
+}
+
+func (h apiHandler) notifyClients(msg Message) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	subscribers, ok := h.subscribers[msg.RoomID]
+
+	if !ok || len(subscribers) == 0 {
+		return
+	}
+
+	for conn, cancel := range subscribers {
+		if err := conn.WriteJSON(msg); err != nil {
+			slog.Error("failed to send message to client", "error", err)
+			cancel()
+		}
+	}
+}
+
 func (h apiHandler) handleSubscribe(w http.ResponseWriter, r *http.Request) {
 	rawRoomID := chi.URLParam(r, "room_id")
 	roomID, err := uuid.Parse(rawRoomID)
